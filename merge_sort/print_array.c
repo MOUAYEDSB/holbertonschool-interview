ece4e07d3bd6a4fa36297f5e1b9ae8570ea4864c
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-07-02 14:10:07 +0100

    update

--- merge_sort/print_array.c
+++ merge_sort/print_array.c
@@ -1,23 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
 
 /**
  * print_array - Prints an array of integers
- *
  * @array: The array to be printed
- * @size: Number of elements in @array
+ * @size: Number of elements in the array
  */
-void print_array(const int *array, size_t size)
-{
+void print_array(const int *array, size_t size) {
     size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
+    if (array == NULL) {
+        printf("NULL array\n");
+        return;
+    }
+
+    for (i = 0; i < size; i++) {
         printf("%d", array[i]);
-        ++i;
+        if (i < size - 1)
+            printf(", ");
     }
     printf("\n");
 }
