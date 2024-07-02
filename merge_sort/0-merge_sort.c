e8b70f42fcf6cfc08118555a0d95433c9579ab71
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-07-02 14:07:20 +0100

    Merge sort

---
+++ merge_sort/0-merge_sort.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * merge - Merges two subarrays of array[]
+ * @array: The array to be sorted
+ * @left: Starting index of left subarray
+ * @mid: Middle index of the array
+ * @right: Ending index of right subarray
+ */
+static void merge(int *array, int left, int mid, int right) {
+    // Implement merging logic
+}
+
+/**
+ * merge_sort - Sorts an array of integers in ascending order using Merge Sort
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void merge_sort(int *array, size_t size) {
+    // Implement merge sort algorithm
+}
