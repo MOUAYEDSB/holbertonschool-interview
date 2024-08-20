0394a6fccd7e14a6c2fb71ad1bc3235bcbc1f453
MOUAYEDSB <sabbaghmouayed@gmail.com>
2024-08-20 21:21:59 +0100

    ok

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" Program that solves the N queens problem."""
+
 import sys
 
+
 def is_safe(queen_positions, row, col):
-    """Checks if the position is safe for a new queen."""
+    """Checks if the position is safe for a new queen"""
     for i in range(row):
         if queen_positions[i] == col or \
            queen_positions[i] - i == col - row or \
@@ -10,8 +13,9 @@ def is_safe(queen_positions, row, col):
             return False
     return True
 
+
 def solve_nqueens(size, row, queen_positions):
-    """Solves the N Queens problem using backtracking."""
+    """ Solves the N Queens problem recursively"""
     if row == size:
         print_solution(queen_positions)
         return
@@ -20,12 +24,14 @@ def solve_nqueens(size, row, queen_positions):
             queen_positions[row] = col
             solve_nqueens(size, row + 1, queen_positions)
 
+
 def print_solution(queen_positions):
-    """Prints one solution in the required format."""
+    """ Prints one solution from the list of queen positions"""
     solution = []
     for row, col in enumerate(queen_positions):
         solution.append([row, col])
     print(solution)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
